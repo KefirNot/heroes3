a136af0791ed564cf7f80305b7b82061cb465f55
Alexandr Nikiforov <kefirnot@gmail.com>
2018-05-28 23:40:29 +0700

    border component refactoring

--- src/component/surface/border.jsx
+++ src/component/surface/border.jsx
@@ -1,70 +1,66 @@
 import React from 'react';
 import './style.scss';
 
-const genStyle = (url) => ({ backgroundImage: `url('${url}')` });
+const HorizontalBorder = (props) => {
+    const { data } = props;
+
+    return (
+        <div className="border-horizontal">
+            {
+                data.map((url, i) => {
+                    const props = {
+                        key      : i,
+                        className: 'border-tile',
+                        style    : { backgroundImage: `url('${url}')` },
+                    }
+                    return <div {...props}></div>;
+                })
+            }
+        </div>
+    );
+}
+
+const VerticalBorder = (props) => {
+    const { data } = props;
+
+    return (
+        <div className="border-vertical">
+            {
+                data.map((url, i) => {
+                    const props = {
+                        key      : i,
+                        className: 'border-tile',
+                        style    : { backgroundImage: `url('${url}')` },
+                    }
+                    return <div {...props}></div>;
+                })
+            }
+        </div>
+    );
+}
 
 export default class extends React.PureComponent {
-	get topBorder() {
-		const { top } = this.props.border;
+    render() {
+        const { size, children, border: { top, right, bottom, left } } = this.props;
+        const mainStyle = {
+            width: (size.width + 2) * 32,
+            height: (size.height + 2) * 32,
+        };
+        const contentStyle = {
+            width: size.width * 32,
+            height: size.height * 32,
+        };
 
-		return (
-			<div className="border-top">
-				{top.map(url => <div className="border-tile" style={genStyle(url)}></div>)}
-			</div>
-		);
-	}
-
-	get bottomBorder() {
-		const { bottom } = this.props.border;
-
-		return (
-			<div className="border-bottom">
-				{bottom.map(url => <div className="border-tile" style={genStyle(url)}></div>)}
-			</div>
-		);
-	}
-
-	get leftBorder() {
-		const { left } = this.props.border;
-
-		return (
-			<div className="border-left">
-				{left.map(url => <div className="border-tile" style={genStyle(url)}></div>)}
-			</div>
-		);
-	}
-
-	get rightBorder() {
-		const { right } = this.props.border;
-
-		return (
-			<div className="border-right">
-				{right.map(url => <div className="border-tile" style={genStyle(url)}></div>)}
-			</div>
-		);
-	}
-
-	render() {
-		const { size, children } = this.props;
-		const mainStyle = {
-			width: (size.width + 2) * 32,
-			height: (size.height + 2) * 32,
-		};
-		const contentStyle = {
-			width: size.width * 32,
-			height: size.height * 32,
-		};
-
-		return (
-			<div className="border" style={mainStyle}>
-				{this.topBorder}
-				{this.leftBorder}
-				<div className="border-content" style={contentStyle}>
-					{children}
-				</div>
-				{this.rightBorder}
-				{this.bottomBorder}
-			</div>
-		);
-	}
+        return (
+            <div className="border" style={mainStyle}>
+                <HorizontalBorder data={top} />
+                <VerticalBorder data={left} />
+                <div className="border-content" style={contentStyle}>
+                    {children}
+                </div>
+                <VerticalBorder data={right} />
+                <HorizontalBorder data={bottom} />
+            </div>
+        );
+    }
 }
